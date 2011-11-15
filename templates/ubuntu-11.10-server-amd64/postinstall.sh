6172215a4307cfcadb7552ffc631e43ffefbdcea
Babar <babar@babar.us>
2011-11-15 20:39:04 +0100

    Use ruby 1.8.7-p352 as p334 has issues with newer openssl librairies

--- templates/ubuntu-11.10-server-amd64/postinstall.sh
+++ templates/ubuntu-11.10-server-amd64/postinstall.sh
@@ -22,14 +22,14 @@ apt-get -y install nfs-common
 # can install their own Rubies using packages or however.
 # We must install the 1.8.x series since Puppet doesn't support
 # Ruby 1.9 yet.
-wget http://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p334.tar.gz
-tar xvzf ruby-1.8.7-p334.tar.gz
-cd ruby-1.8.7-p334
+wget http://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p352.tar.gz
+tar xvzf ruby-1.8.7-p352.tar.gz
+cd ruby-1.8.7-p352
 ./configure --prefix=/opt/ruby
 make
 make install
 cd ..
-rm -rf ruby-1.8.7-p334*
+rm -rf ruby-1.8.7-p352*
 
 # Install RubyGems 1.7.2
 wget http://production.cf.rubygems.org/rubygems/rubygems-1.7.2.tgz
